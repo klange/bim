31b58861e46c74e3b8e46f55f80c16a5df6404f0
K. Lange <klange@toaruos.org>
2019-06-22 15:39:09 +0900

    Python highlighting with new highlighter

--- bim.c
+++ bim.c
@@ -960,12 +960,172 @@ static int syn_c_calculate(struct syntax_state * state) {
 
 static char * c_ext[] = {".c",".h",".cpp",".hpp",".c++",".h++",NULL};
 
+static char * syn_py_keywords[] = {
+	"class","def","return","del","if","else","elif","for","while","continue",
+	"break","assert","as","and","or","except","finally","from","global",
+	"import","in","is","lambda","with","nonlocal","not","pass","raise","try","yield",
+	NULL
+};
+
+static char * syn_py_types[] = {
+	"object","set","dict","int","str","bytes",
+	NULL
+};
+
+static char * syn_py_special[] = {
+	"True","False","None",
+	NULL
+};
+
+static int paint_py_triple_double(struct syntax_state * state) {
+	while (charat() != -1) {
+		if (charat() == '"') {
+			paint(1, FLAG_STRING);
+			if (charat() == '"' && nextchar() == '"') {
+				paint(2, FLAG_STRING);
+				return 0;
+			}
+		} else {
+			paint(1, FLAG_STRING);
+		}
+	}
+	return 1; /* continues */
+}
+
+static int paint_py_triple_single(struct syntax_state * state) {
+	while (charat() != -1) {
+		if (charat() == '\'') {
+			paint(1, FLAG_STRING);
+			if (charat() == '\'' && nextchar() == '\'') {
+				paint(2, FLAG_STRING);
+				return 0;
+			}
+		} else {
+			paint(1, FLAG_STRING);
+		}
+	}
+	return 2; /* continues */
+}
+
+static int paint_py_single_string(struct syntax_state * state) {
+	paint(1, FLAG_STRING);
+	int last = -1;
+	while (charat() != -1) {
+		if (last != '\\' && charat() == '\'') {
+			paint(1, FLAG_STRING);
+			return 0;
+		} else if (last == '\\' && charat() == '\\') {
+			paint(1, FLAG_STRING);
+			last = -1;
+			/* TODO check for \0, \r, \n, \0xxx, etc. */
+		} else {
+			last = charat();
+			paint(1, FLAG_STRING);
+		}
+	}
+	return 0;
+}
+
+static int paint_py_numeral(struct syntax_state * state) {
+	if (charat() == '0' && (nextchar() == 'x' || nextchar() == 'X')) {
+		paint(2, FLAG_NUMERAL);
+		while (isxdigit(charat())) paint(1, FLAG_NUMERAL);
+	} else if (charat() == '0' && nextchar() == '.') {
+		paint(2, FLAG_NUMERAL);
+		while (isdigit(charat())) paint(1, FLAG_NUMERAL);
+		if ((charat() == '+' || charat() == '-') && (nextchar() == 'e' || nextchar() == 'E')) {
+			paint(2, FLAG_NUMERAL);
+			while (isdigit(charat())) paint(1, FLAG_NUMERAL);
+		} else if (charat() == 'e' || charat() == 'E') {
+			paint(1, FLAG_NUMERAL);
+			while (isdigit(charat())) paint(1, FLAG_NUMERAL);
+		}
+		if (charat() == 'j') paint(1, FLAG_NUMERAL);
+		return 0;
+	} else {
+		while (isdigit(charat())) paint(1, FLAG_NUMERAL);
+		if (charat() == '.') {
+			paint(1, FLAG_NUMERAL);
+			while (isdigit(charat())) paint(1, FLAG_NUMERAL);
+			if ((charat() == '+' || charat() == '-') && (nextchar() == 'e' || nextchar() == 'E')) {
+				paint(2, FLAG_NUMERAL);
+				while (isdigit(charat())) paint(1, FLAG_NUMERAL);
+			} else if (charat() == 'e' || charat() == 'E') {
+				paint(1, FLAG_NUMERAL);
+				while (isdigit(charat())) paint(1, FLAG_NUMERAL);
+			}
+			if (charat() == 'j') paint(1, FLAG_NUMERAL);
+			return 0;
+		}
+		if (charat() == 'j') paint(1, FLAG_NUMERAL);
+	}
+	while (charat() == 'l' || charat() == 'L') paint(1, FLAG_NUMERAL);
+	return 0;
+}
+
+static int syn_py_calculate(struct syntax_state * state) {
+	while (1) {
+		switch (state->state) {
+			case -1:
+			case 0:
+				if (charat() == '#') {
+					paint_comment(state);
+				} else if (state->i == 0 && match_and_paint(state, "import", FLAG_PRAGMA, c_keyword_qualifier)) {
+					return 0;
+				} else if (charat() == '@') {
+					paint(1, FLAG_PRAGMA);
+					while (c_keyword_qualifier(charat())) paint(1, FLAG_PRAGMA);
+					return 0;
+				} else if (charat() == '"') {
+					paint(1, FLAG_STRING);
+					if (charat() == '"' && nextchar() == '"') {
+						paint(2, FLAG_STRING);
+						return paint_py_triple_double(state);
+					} else {
+						paint_c_string(state);
+						return 0;
+					}
+				} else if (find_keywords(state, syn_py_keywords, FLAG_KEYWORD, c_keyword_qualifier)) {
+					return 0;
+				} else if (find_keywords(state, syn_py_types, FLAG_TYPE, c_keyword_qualifier)) {
+					return 0;
+				} else if (find_keywords(state, syn_py_special, FLAG_NUMERAL, c_keyword_qualifier)) {
+					return 0;
+				} else if (charat() == '\'') {
+					paint(1, FLAG_STRING);
+					if (charat() == '\'' && nextchar() == '\'') {
+						paint(2, FLAG_STRING);
+						return paint_py_triple_single(state);
+					} else {
+						paint_py_single_string(state);
+						return 0;
+					}
+				} else if (!c_keyword_qualifier(lastchar()) && isdigit(charat())) {
+					paint_py_numeral(state);
+					return 0;
+				} else if (charat() != -1) {
+					skip();
+					return 0;
+				}
+				break;
+			case 1: /* multiline """ string */
+				return paint_py_triple_double(state);
+			case 2: /* multiline ''' string */
+				return paint_py_triple_single(state);
+		}
+		return -1;
+	}
+}
+
+static char * py_ext[] = {".py",NULL};
+
 struct syntax_definition {
 	char * name;
 	char ** ext;
 	int (*calculate)(struct syntax_state *);
 } syntaxes[] = {
 	{"c",c_ext,syn_c_calculate},
+	{"python",py_ext,syn_py_calculate},
 	{NULL,NULL,NULL},
 };
 
