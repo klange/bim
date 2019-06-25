8303aa5b57b5c2225d33ec9ea85353f5dba803cd
K. Lange <klange@toaruos.org>
2019-06-25 12:02:20 +0900

    rudimentary protobuf highlighting

--- bim.c
+++ bim.c
@@ -1951,6 +1951,58 @@ _comment:
 
 static char * xml_ext[] = {".xml",".htm",".html",NULL}; // TODO other stuff that uses xml (it's a lot!); FIXME htm/html are temporary; make dedicated SGML ones for this
 
+static char * syn_proto_keywords[] = {
+	"syntax","import","option","package","message","group","oneof",
+	"optional","required","repeated","default","extend","extensions","to","max","reserved",
+	"service","rpc","returns","stream",
+	NULL
+};
+
+static char * syn_proto_types[] = {
+	"int32","int64","uint32","uint64","sint32","sint64",
+	"fixed32","fixed64","sfixed32","sfixed64",
+	"float","double","bool","string","bytes",
+	"enum",
+	NULL
+};
+
+static char * syn_proto_special[] = {
+	"true","false",
+	NULL
+};
+
+static int syn_proto_calculate(struct syntax_state * state) {
+	if (state->state < 1) {
+		if (charat() == '/' && nextchar() == '/') {
+			paint_comment(state);
+		} else if (charat() == '/' && nextchar() == '*') {
+			if (paint_c_comment(state) == 1) return 1;
+			return 0;
+		} else if (find_keywords(state, syn_proto_keywords, FLAG_KEYWORD, c_keyword_qualifier)) {
+			return 0;
+		} else if (find_keywords(state, syn_proto_types, FLAG_TYPE, c_keyword_qualifier)) {
+			return 0;
+		} else if (find_keywords(state, syn_proto_special, FLAG_NUMERAL, c_keyword_qualifier)) {
+			return 0;
+		} else if (charat() == '"') {
+			paint_simple_string(state);
+			return 0;
+		} else if (!c_keyword_qualifier(lastchar()) && isdigit(charat())) {
+			paint_c_numeral(state);
+			return 0;
+		} else if (charat() != -1) {
+			skip();
+			return 0;
+		}
+		return -1;
+	} else {
+		if (paint_c_comment(state) == 1) return 1;
+		return 0;
+	}
+}
+
+static char * proto_ext[] = {".proto",NULL};
+
 struct syntax_definition {
 	char * name;
 	char ** ext;
@@ -1970,6 +2022,7 @@ struct syntax_definition {
 	{"markdown",markdown_ext,syn_markdown_calculate,1},
 	{"json",json_ext,syn_json_calculate,1},
 	{"xml",xml_ext,syn_xml_calculate,1},
+	{"protobuf",proto_ext,syn_proto_calculate,1},
 	{NULL,NULL,NULL,0},
 };
 
